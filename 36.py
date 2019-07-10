252b42970a88eb41c5cdb542bc01960b01c1f9ce
Benjamin Shi <aiifabbf@outlook.com>
2019-07-10 15:14:37 +0800

    36 improve: iterate only once

--- 36.py
+++ 36.py
@@ -15,53 +15,74 @@ from typing import *
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 
-        for rowIndex, row in enumerate(board): # 检查每行是否有重复数字
-            numbersInThisRow = set()
+        # for rowIndex, row in enumerate(board): # 检查每行是否有重复数字
+        #     numbersInThisRow = set()
+
+        #     for columnIndex, box in enumerate(row):
+        #         if box == ".":
+        #             continue
+        #         else:
+        #             if box in numbersInThisRow:
+        #                 return False # 这行出现了重复数字
+        #             else:
+        #                 numbersInThisRow.add(box)
+
+        # for columnIndex in range(9): # 检查每列是否有重复数字
+        #     numbersInThisColumn = set()
+
+        #     for rowIndex in range(9):
+        #         box = board[rowIndex][columnIndex]
+        #         if box == ".":
+        #             continue
+        #         else:
+        #             if box in numbersInThisColumn:
+        #                 return False # 这列出现了重复数字
+        #             else:
+        #                 numbersInThisColumn.add(box)
+
+        # subBoxes = [
+        #     (0, 0),
+        #     (0, 3),
+        #     (0, 6),
+        #     (3, 0),
+        #     (3, 3),
+        #     (3, 6),
+        #     (6, 0),
+        #     (6, 3),
+        #     (6, 6)
+        # ] # 每宫的左上角的位置
+
+        # for subbox in subBoxes: # 检查每宫里是否含有重复数字
+        #     indexes = [(i + subbox[0], j + subbox[1]) for i in range(3) for j in range(3)] # 这一宫里所有数字的位置
+        #     boxes = filter(lambda v: v != ".", map(lambda w: board[w[0]][w[1]], indexes))
+        #     numbersInThisSubbox = set()
+
+        #     for box in boxes:
+        #         if box in numbersInThisSubbox:
+        #             return False # 这一宫里出现了重复数字
+        #         else:
+        #             numbersInThisSubbox.add(box)
+
+        # return True
+
+        # 看排名还看到了只遍历一次的神仙做法，大致思路是用3个set，分别专门用来判断行重复、列重复和宫重复。
+
+        # 但是速度和上面遍历3次的没差别……可能是python overhead的问题吧
+
+        seenInRow = set() # 专门用来判断行重复的set，里面存行号、数字对
+        seenInColumn = set() # 存列号、数字对
+        seenInSubbox = set() # 存宫号、数字对
+
+        for rowIndex, row in enumerate(board):
 
             for columnIndex, box in enumerate(row):
-                if box == ".":
-                    continue
-                else:
-                    if box in numbersInThisRow:
-                        return False # 这行出现了重复数字
-                    else:
-                        numbersInThisRow.add(box)
+                if box != ".":
 
-        for columnIndex in range(9): # 检查每列是否有重复数字
-            numbersInThisColumn = set()
-
-            for rowIndex in range(9):
-                box = board[rowIndex][columnIndex]
-                if box == ".":
-                    continue
-                else:
-                    if box in numbersInThisColumn:
-                        return False # 这列出现了重复数字
-                    else:
-                        numbersInThisColumn.add(box)
-
-        subBoxes = [
-            (0, 0),
-            (0, 3),
-            (0, 6),
-            (3, 0),
-            (3, 3),
-            (3, 6),
-            (6, 0),
-            (6, 3),
-            (6, 6)
-        ] # 每宫的左上角的位置
-
-        for subbox in subBoxes: # 检查每宫里是否含有重复数字
-            indexes = [(i + subbox[0], j + subbox[1]) for i in range(3) for j in range(3)] # 这一宫里所有数字的位置
-            boxes = filter(lambda v: v != ".", map(lambda w: board[w[0]][w[1]], indexes))
-            numbersInThisSubbox = set()
-
-            for box in boxes:
-                if box in numbersInThisSubbox:
-                    return False # 这一宫里出现了重复数字
-                else:
-                    numbersInThisSubbox.add(box)
+                    for v, s in zip([(rowIndex, box), (columnIndex, box), (rowIndex // 3, columnIndex // 3, box)], [seenInRow, seenInColumn, seenInSubbox]):
+                        if v in s:
+                            return False
+                        else:
+                            s.add(v)
 
         return True
 
