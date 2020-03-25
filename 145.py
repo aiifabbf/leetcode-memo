f37d684527945811d464904048cd4196412db855
Benjamin Shi <aiifabbf@outlook.com>
2020-03-24 20:11:02 -0500

    145 stack

--- 145.py
+++ 145.py
@@ -1,4 +1,6 @@
-# 二叉树的后根遍历
+"""
+二叉树的后根遍历
+"""
 
 from typing import *
 
@@ -10,15 +12,33 @@ from typing import *
 #         self.right = None
 
 class Solution:
+    # def postorderTraversal(self, root: TreeNode) -> List[int]:
+    #     if root:
+    #         res = []
+    #         if root.left:
+    #             res += self.postorderTraversal(root.left)
+    #         if root.right:
+    #             res += self.postorderTraversal(root.right)
+    #         res.append(root.val)
+
+    #         return res
+    #     else:
+    #         return []
+
     def postorderTraversal(self, root: TreeNode) -> List[int]:
         if root:
+            stack = [root]
             res = []
-            if root.left:
-                res += self.postorderTraversal(root.left)
-            if root.right:
-                res += self.postorderTraversal(root.right)
-            res.append(root.val)
 
-            return res
+            while stack:
+                node = stack.pop()
+                if node.left: # 先放左边
+                    stack.append(node.left)
+                if node.right: # 再放右边
+                    stack.append(node.right)
+
+                res.append(node)
+
+            return res[:: -1] # 最后颠倒
         else:
             return []
