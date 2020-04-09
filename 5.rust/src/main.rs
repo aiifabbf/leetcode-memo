e016d601b67497f3963ca96bf91073c3530007dd
Benjamin Shi <aiifabbf@outlook.com>
2020-04-09 16:13:12 -0500

    5 improve rust version: replace vec with slice

--- 5.rust/src/main.rs
+++ 5.rust/src/main.rs
@@ -2,35 +2,40 @@ struct Solution;
 
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
+        if s.len() == 0 {
+            return "".to_string();
+        }
+
         let s: Vec<char> = s.chars().collect();
         let mut dp: Vec<Vec<bool>> = vec![vec![false; s.len() + 1]; s.len() + 1]; // dp[i][j] = true表示s[i..j]是回文substring
-        let mut res: Vec<char> = Vec::new(); // 目前为止见到的最长的回文substring
+        let mut res: &[char] = &[]; // 目前为止见到的最长的回文substring。这里改成slice了，速度和空间都节省很多。原来用的是Vec。
 
-        for i in 0..dp.len() { // 初始条件s[i..i]是空字符串，所以肯定是回文string
+        for i in 0..dp.len() {
+            // 初始条件s[i..i]是空字符串，所以肯定是回文string
             dp[i][i] = true;
         }
 
-        for i in 0..dp.len() - 1 { // 初始条件s[i..i+1]是只有一个字符的字符串，所以肯定也是回文的
+        for i in 0..dp.len() - 1 {
+            // 初始条件s[i..i+1]是只有一个字符的字符串，所以肯定也是回文的
             dp[i][i + 1] = true;
         }
-        
-        if s.len() > 0 {
-            res.push(s[0]); // 目前为止见到的最长的回文substring就是其中一个
-        }
 
-        for i in (0..dp.len()).rev() { // i是倒过来、从下往上遍历的
-            for j in i + 2..dp[0].len() { // s[i..i+1]已经是初始条件了，所以不用管了，直接从s[i..i+2]开始就行了
+        res = &s[0..1];
+
+        for i in (0..dp.len()).rev() {
+            // i是倒过来、从下往上遍历的
+            for j in i + 2..dp[0].len() {
+                // s[i..i+1]已经是初始条件了，所以不用管了，直接从s[i..i+2]开始就行了
                 if s[i] == s[j - 1] && dp[i + 1][j - 1] == true {
                     dp[i][j] = true;
                     if j - i > res.len() {
-                        res.clear();
-                        res.extend(s[i..j].iter());
+                        res = &s[i..j];
                     }
                 }
             }
         }
 
-        return res.into_iter().collect::<String>(); // 很奇怪Vec<char>居然没有join方法
+        return res.iter().collect(); // 不需要collect::<String>()，会根据函数返回值类型推导的
     }
 
     // 最长回文substring问题不能像最长回文subsequence转换到最长公共subsequence那样、转化到最长公共substring
@@ -63,5 +68,5 @@ pub fn main() {
         "{:?}",
         Solution::longest_palindrome("aacdefcaa".to_string())
     ); // aa
-    // 这是最著名的反例
+       // 这是最著名的反例
 }
